9aaee2c0e94f9973d60f681eeb2163a8b990a7ff
Moh0401 <161856130+Moh0401@users.noreply.github.com>
2024-07-26 20:13:25 +0000

    20:13
    FIRST COMMIT

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Banner from './Components/Banner'
+import Cart from './Components/Cart'
+import ShoppingList from "./Components/ShoppingList";
+
+function App() {
+  return <div><Banner /><Cart /><ShoppingList /></div>
+
+}
+
+export default App;
