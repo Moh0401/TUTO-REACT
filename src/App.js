601dd31cde1a338c79e62c784560e75cb703aa7f
Moh0401 <161856130+Moh0401@users.noreply.github.com>
2024-07-28 18:44:36 +0000

    18:44
    2e commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import './App.css';
 import Banner from './Components/Banner'
 import Cart from './Components/Cart'
 import ShoppingList from "./Components/ShoppingList";
+//import QuestionForm from "./Components/QuestionForm";
+import Footer from "./Components/Footer";
+import { useState } from 'react';
+import logo from './assets/logo.png'
+import './Styles/Layout.css'
 
 function App() {
-  return <div><Banner /><Cart /><ShoppingList /></div>
-
+	const [cart, updateCart] = useState([])
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
